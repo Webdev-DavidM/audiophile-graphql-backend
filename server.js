c504a48bbddd96aced1ed1c44f079fb452c58acc
David <>
2022-03-14 20:51:25 +0000

    deploy

--- server.js
+++ server.js
@@ -1,33 +1,14 @@
-import { ApolloServer } from 'apollo-server-express';
-import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
-import { resolvers } from './graphql/resolvers.js';
-import { typeDefs } from './graphql/typeDefs.js';
-import express from 'express';
-import http from 'http';
-import path from 'path';
+import { ApolloServer } from 'apollo-server';
 import connectDB from './config/db.js';
+import { typeDefs } from './graphql/typeDefs.js';
+import { resolvers } from './graphql/resolvers.js';
 
-async function startApolloServer() {
-  const app = express();
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({ typeDefs, resolvers });
 
-  const httpServer = http.createServer(app);
-
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
-  });
-
-  await server.start();
-  app.use(express.static('public'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
-  });
+// The `listen` method launches a web server.
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
   connectDB();
-  server.applyMiddleware({ app });
-  let port = process.env.PORT | 4000;
-  await new Promise((resolve) => httpServer.listen(port, resolve));
-  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
-}
-
-startApolloServer();
+  console.log(`🚀 Server ready at ${url}`);
+});
