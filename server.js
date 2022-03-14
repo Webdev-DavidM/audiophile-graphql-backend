dc32537d8296dd08e73d597fc624c44fa3a8ce04
David <>
2022-03-14 18:15:14 +0000

    react router basename added

--- server.js
+++ server.js
@@ -4,25 +4,12 @@ import { typeDefs } from './graphql/typeDefs.js';
 import { resolvers } from './graphql/resolvers.js';
 import express, { application } from 'express';
 
-import cors from 'cors';
-
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
 const server = new ApolloServer({ typeDefs, resolvers });
 
-// const app = express();
-// app.use(express.static('public'));
-// app.get('*', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
-// });
-
 // The `listen` method launches a web server.
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
   connectDB();
-  const app = express();
-  app.use(express.static('public'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
-  });
   console.log(`🚀 Server ready at ${url}`);
 });
