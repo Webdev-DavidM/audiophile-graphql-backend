f544c8e95b0e97d5074f5603877421af5416ee9a
David <>
2022-03-14 19:19:43 +0000

    deploy

--- server.js
+++ server.js
@@ -1,25 +1,27 @@
-import { ApolloServer } from 'apollo-server';
-import connectDB from './config/db.js';
-import { typeDefs } from './graphql/typeDefs.js';
+import { ApolloServer } from 'apollo-server-express';
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
 import { resolvers } from './graphql/resolvers.js';
+import { typeDefs } from './graphql/typeDefs.js';
 import express from 'express';
+import http from 'http';
+import path from 'path';
 
-import cors from 'cors';
+async function startApolloServer() {
+  const app = express();
 
-app.use(cors());
+  const httpServer = http.createServer(app);
 
-const server = new ApolloServer({ typeDefs, resolvers });
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
 
-const app = express();
-app.use(express.static('public'));
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
-});
+  await server.start();
+  app.get('*', express.static('public'));
+  server.applyMiddleware({ app });
+  await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+}
 
-server.applyMiddleware({ app });
-
-const port = process.env.PORT || 4000;
-
-app.listen(port, () =>
-  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
-);
+startApolloServer();
