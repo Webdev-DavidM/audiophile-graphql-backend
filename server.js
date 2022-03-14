3bc2f8b4dced4097f3d4dc42b64c8d794e4baf0c
David <>
2022-03-14 18:33:16 +0000

    deploy

--- server.js
+++ server.js
@@ -2,14 +2,26 @@ import { ApolloServer } from 'apollo-server';
 import connectDB from './config/db.js';
 import { typeDefs } from './graphql/typeDefs.js';
 import { resolvers } from './graphql/resolvers.js';
-import express, { application } from 'express';
+import express from 'express';
 
-// The ApolloServer constructor requires two parameters: your schema
-// definition and your set of resolvers.
-const server = new ApolloServer({ typeDefs, resolvers });
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+app.use('/graphql', (req, res) => {
+  // The ApolloServer constructor requires two parameters: your schema
+  // definition and your set of resolvers.
+  const server = new ApolloServer({ typeDefs, resolvers });
+});
+
+app.use(express.static('public'));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
 
 // The `listen` method launches a web server.
-server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
-  connectDB();
-  console.log(`🚀 Server ready at ${url}`);
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+  console.log('listening on port', port);
 });
